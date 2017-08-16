27ebdf3aac488c1eaf377d6437f538d70be4168f
jim_ <769925821@qq.com>
2017-08-16 22:34:08 +0800

    调整

--- index.js
+++ index.js
@@ -1,37 +1,43 @@
 
 const lib = require('./lib')
 
+const log =  (...args) => console.log.apply(null, [new Date().toLocaleString(), ...args])
+
 async function main() {
-    console.log('正在加载图片模板')
+    log('正在加载图片模板')
     const pd = await lib.getPixelsData('canvas.png', '#646464')
-    console.log('图片模板加载完成，需要校正的色块有： ', pd.length)
-    console.log()
+    log('图片模板加载完成，需要校正的色块有： ', pd.length)
     check()
     async function check() {
-        console.log('校正中...')
-        pd.sort(() => Math.random())
-        const bitmap = await lib.getBitmap()
-        for(const [x, y, colorName] of pd) {
-            const bn = bitmap[x + y * 1280]
-            if (bn !== colorName) {
-                console.log(`需要校正${x}, ${y} ${bn} --> ${colorName}`)
-                const result = await lib.drawData(x, y, colorName)
-                if (result.code === 0) {
-                    console.log(`校正成功，下次校正时间为${result.data.time}秒后`)
-                    setTimeout(check, (result.data.time + 2) * 1000)
-                } else {
-                    console.log('校正失败: ', result)
-                    if (result.data.time) {
+        try {
+            log('校正中...')
+            pd.sort(() => Math.random())
+            const bitmap = await lib.getBitmap()
+            for(const [x, y, colorName] of pd) {
+                const bn = bitmap[x + y * 1280]
+                if (bn !== colorName) {
+                    log(`需要校正${x}, ${y} ${bn} --> ${colorName}`)
+                    const result = await lib.drawData(x, y, colorName)
+                    if (result.code === 0) {
+                        log(`校正成功，下次校正时间为${result.data.time}秒后`)
                         setTimeout(check, (result.data.time + 2) * 1000)
                     } else {
-                        setTimeout(check, 180 * 1000)
+                        log('校正失败: ', result)
+                        if (result.data.time) {
+                            setTimeout(check, (result.data.time + 2) * 1000)
+                        } else {
+                            setTimeout(check, 180 * 1000)
+                        }
                     }
+                    return
                 }
-                return
             }
+            log('完全匹配！')
+            setTimeout(check, 60 * 1000)
+        } catch (error) {
+            console.warn(error)
+            check()
         }
-        console.log('完全匹配！')
-        setTimeout(check, 60 * 1000)
     }
 }
 
